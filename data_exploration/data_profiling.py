16d0049f0f10a93669d49f637108e3a9659d7f58
C Matteo <138587358+cmatteogr@users.noreply.github.com>
2024-06-21 09:00:21 -0500

    Code formatted, description comments added

--- data_exploration/data_profiling.py
+++ data_exploration/data_profiling.py
@@ -1,10 +1,7 @@
-
 import pandas as pd
 from ydata_profiling import ProfileReport
 
-
-# df = pd.read_csv(r'C:\Users\cesar_0qb0xal\Documents\GitHub\cars_ml_project\data\data_exploration\input\cars.csv')
-df = pd.read_csv(r'C:\Users\cesar_0qb0xal\Documents\GitHub\cars_ml_project\data\preprocess\cars_prepared.csv')
+df = pd.read_csv(r'.\data\preprocess\cars_prepared.csv')
 
 profile = ProfileReport(df, title="Cars Profiling Report")
 profile.to_file("cars_report_prepared.html")
